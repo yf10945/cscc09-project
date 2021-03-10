ec45820eafac32d63c7cad04648ad5287c6fcbae
Yishen Feng <71033521+yf10945@users.noreply.github.com>
2021-03-10 11:33:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
