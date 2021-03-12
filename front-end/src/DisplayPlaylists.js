fb18f196e5f1585a3ce771e30905c0beb06ad594
Glenn Ye <glenn.ye38@gmail.com>
2021-03-11 23:25:30 -0500

    Add basic playlist page

---
+++ front-end/src/DisplayPlaylists.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function DisplayPlaylists() {
+    return (
+        <div className="DisplayPlaylists">
+            <h1>My Playlists</h1>
+        </div>
+      );
+}
