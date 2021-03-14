42a5c93054204e61d5a63801daea8bb0bbf0293f
yf10945 <71033521+yf10945@users.noreply.github.com>
2021-03-13 23:54:50 -0500

    fix nav bar pages

--- front-end/src/components/AboutUsPage.js
+++ front-end/src/components/AboutUsPage.js
@@ -2,29 +2,24 @@ import React from 'react';
 import "../styles.css";
 import "./AboutUsPage.css";
 import { Link } from 'react-router-dom';
-import NavBar from "./NavBar";
 
 export default function AboutUsPage() {
   return (
-    <div>
-      <NavBar/>
-      <div className="about-us-page main-theme main">
-        
-          <img
-            src="http://cdn.onlinewebfonts.com/svg/img_496903.png"
-            alt="logo"
-            className="icon"
-          />
-          <h1>About Us</h1>
-          <p>
-            This app is developed for CSCC09 project. It is a music player app that
-            allows for karaoke.
-          </p>
-          <p>Developed by: Yishen Feng, Glenn Qing Yuan Ye, and Kia Naderi</p>
-          <Link to="/" className="back-to-welcome undecorated-link decorated-when-hovered-link">
-            <p className="main-theme-no-background back-to-welcome">Back to Home</p>
-          </Link>  
-      </div>
+    <div className="about-us-page main-theme">
+      <img
+        src="http://cdn.onlinewebfonts.com/svg/img_496903.png"
+        alt="logo"
+        className="icon"
+      />
+      <h1>About Us</h1>
+      <p>
+        This app is developed for CSCC09 project. It is a music player app that
+        allows for karaoke.
+      </p>
+      <p>Developed by: Yishen Feng, Glenn Qing Yuan Ye, and Kia Naderi</p>
+      <Link to="/" className="back-to-welcome undecorated-link decorated-when-hovered-link">
+        <p className="main-theme-no-background back-to-welcome">Back to Home</p>
+      </Link>   
     </div>
   );
 }
--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -7,7 +7,6 @@ export default function NavBar() {
     return (
         <div className="NavContainer main-theme">
             <ul>
-                <li><Link to="/aboutus"> About Us </Link></li>
                 <li><Link to="/playlists"> My Playlists </Link></li>
                 <li><Link to="/addsong"> Add Song </Link></li>
             </ul>
