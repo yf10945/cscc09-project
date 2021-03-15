087206d58cd5041e8482fa4bd8df38a3fe5eddfb
yf10945 <71033521+yf10945@users.noreply.github.com>
2021-03-15 15:06:56 -0400

    update s3 configs

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ const { buildSchema } = require('graphql');
 const aws = require('aws-sdk');
 aws.config.region = 'us-east-2';
 const S3_BUCKET = process.env.S3_BUCKET;
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
 
 const uri = "mongodb+srv://admin:mongo@cluster0.z0caj.mongodb.net/project?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true }, (err) => {
@@ -85,7 +90,6 @@ app.post('/login', passport.authenticate('local'), (req, res) => {
 });
 
 app.get('/sign-s3', (req, res) => {
-  const s3 = new aws.S3();
   const fileName = req.query['file-name'];
   const fileType = req.query['file-type'];
   const s3Params = {
