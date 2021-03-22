badf942f453e6b2b236a129cbd4e404814f0b26c
Glenn Ye <glenn.ye38@gmail.com>
2021-03-21 20:19:22 -0400

    Add basic songs page

--- front-end/src/components/Burger.styled.js
+++ front-end/src/components/Burger.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const StyledBurger = styled.button`
-    position: absolute;
+    position: fixed;
     top: 2%;
     left: 1rem;
     display: flex;
