fe872853cbe06ea3d72a5fb3357b6bf72db94594
Glenn Ye <glenn.ye38@gmail.com>
2021-04-05 22:57:59 -0400

    Add create playlists form frontend

--- front-end/src/pages/PlaylistsPage.js
+++ front-end/src/pages/PlaylistsPage.js
@@ -4,11 +4,21 @@ import "./PlaylistsPage.css";
 import { useDataLayerValue } from "../dataLayer";
 import PlaylistList from "../components/PlaylistList";
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import { Dialog, DialogActions, DialogContent, DialogTitle, TextField, Button } from "@material-ui/core";
 
 
 function PlaylistsPage() {
     const [{ user }, dispatch] = useDataLayerValue();
     const [playlists, setPlaylists] = useState([]);
+    const [open, setOpen] = useState(false);
+
+    const openCreatePlaylists = () => {
+        setOpen(true);
+    };
+
+    const closeCreatePlaylists = () => {
+        setOpen(false);
+    };  
 
     const getPlaylists = (username) => {
         fetch("/graphql", {
@@ -51,6 +61,10 @@ function PlaylistsPage() {
         .catch(error => console.log(error));
     };
 
+    const createPlaylist = () => {
+        
+    };
+
     useEffect(() => {
         getPlaylists(user);
     }, [user]);
@@ -59,10 +73,42 @@ function PlaylistsPage() {
         <div className="playlists-page main-theme">
             <div className="main">
                 <h1>Your Playlists</h1>
-                <div className="create-playlist">
+                <div className="create-playlist" onClick={openCreatePlaylists}>
                     <AddCircleOutlineIcon fontSize="large" />
                     <div className="create-playlist-text">Create Playlist</div>
                 </div>
+                <Dialog
+                    open={open}
+                    onClose={closeCreatePlaylists} 
+                    className="create-playlists-form"
+                    PaperProps={{
+                        style: {
+                            borderRadius: "10px",
+                            width: "30vw",
+                            height: "30vh",
+                        },
+                    }}>
+                    <DialogTitle className="create-playlists-form-title">Create Playlist</DialogTitle>
+                    <DialogContent>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="playlist-name"
+                            label="Playlist Name"
+                            type="text"
+                            fullWidth={true}
+                            className="create-playlist-form-label"
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={closeCreatePlaylists} className="form-button">
+                            Cancel
+                        </Button>
+                        <Button onClick={createPlaylist} className="form-button">
+                            Create
+                        </Button>
+                    </DialogActions>
+                </Dialog>
                 <PlaylistList playlists={playlists} setPlaylists={setPlaylists} />
                 {/* Add spacing between playlists list and music control bar with footer*/}
                 <div className="footer"></div>
