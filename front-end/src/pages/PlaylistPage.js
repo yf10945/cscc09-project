6a5071437ceeedfe1b5ba5ef323b8b266f2b0c02
Glenn Ye <glenn.ye38@gmail.com>
2021-04-08 15:54:58 -0400

    Add add songs to playlists

--- front-end/src/pages/PlaylistPage.js
+++ front-end/src/pages/PlaylistPage.js
@@ -1,21 +1,25 @@
 import React, { useState, useEffect } from "react";
 import "../styles.css";
 import "./PlaylistPage.css";
+import AddSongList from "../components/AddSongList";
 import SongList from "../components/SongList";
 import { useDataLayerValue } from "../dataLayer";
 import { useLocation } from "react-router-dom";
 import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
 // import CloseIcon from '@material-ui/icons/Close';
-import { Dialog, DialogActions, DialogTitle, Button, IconButton } from "@material-ui/core";
+import { Dialog, DialogActions, DialogTitle, Button, IconButton, DialogContent } from "@material-ui/core";
 import { useHistory } from 'react-router-dom';
+import { SignalCellular0Bar } from "@material-ui/icons";
 // import Snackbar from '@material-ui/core/Snackbar';
 
 
 function PlaylistPage() {
     const [{ playingPlaylist, viewingPlaylist, songlist, playingSong, playingSongTitle, playingSongArtists }, dispatch] = useDataLayerValue();
     const [songs, setSongs] = useState([]);
+    const [addSongs, setAddSongs] = useState([]);
     const [openDelete, setOpenDelete] = useState(false);
+    const [openAdd, setOpenAdd] = useState(false);
     // const [deleteSuccess, setDeleteSuccess] = useState(false);
     const location = useLocation();
     const playlistId = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
@@ -161,12 +165,110 @@ function PlaylistPage() {
         .catch(error => console.log(error));
     };
 
+    const addSongToPlaylist = (songId, playlistId) => {
+        fetch("/graphql", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                query: `
+                    mutation {
+                        addSongToPlaylist(songId: "${songId}", playlistId: "${playlistId}") {
+                            _id
+                            title
+                            user
+                            songs {
+                                _id
+                                songName
+                                artist
+                                filepath
+                                lyrics
+                            }                               
+                        }
+                    }
+                `
+            }),
+        })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(response.statusText);
+            }
+        })
+        .then((response) => {
+            getPlaylistSongs();
+        })
+        .catch(error => console.log(error));
+    };
+
+    const getAddSongs = () => {
+        fetch('/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                query: `
+                    query {
+                        getAllSongs {
+                            _id
+                            songName
+                            artist
+                            filepath
+                            lyrics
+                        }
+                    }
+                `,
+            })
+        })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(response.statusText);
+            }
+        })
+        .then((response) => {
+            // console.log(response.data.getAllSongs);
+            let validSongs = [];
+            for (let i = 0; i < response.data.getAllSongs.length; i++) {
+                // console.log(songlist[i].filepath);
+                // console.log(response.data.getAllSongs[i].filepath);
+                // console.log(songlist);
+                // console.log(response.data.getAllSongs);
+                // console.log(i);
+                // console.log(songlist.find(j => j.filepath === url));
+                let url = response.data.getAllSongs[i].filepath;
+                if (songlist.find(j => j.filepath === url) === undefined) {
+                    validSongs.push(response.data.getAllSongs[i]);
+                }
+            }
+            // console.log(validSongs);
+            setAddSongs(validSongs);
+        })
+        .catch(error => console.log(error));
+    };
+
     const openConfirmDeletePlaylist = () => {
         setOpenDelete(true);
     };
 
     const closeConfirmDeletePlaylist = () => {
         setOpenDelete(false);
+    };
+
+    const openAddSongDialog = () => {
+        getAddSongs();
+        setOpenAdd(true);
+        // console.log(addSongs);
+    };
+
+    const closeAddSongDialog = () => {
+        setOpenAdd(false);
     };
 
     // const openDeleteSuccess = () => {
@@ -181,6 +283,10 @@ function PlaylistPage() {
     useEffect(() => {
         getPlaylistSongs();
     }, []);
+
+    useEffect(() => {
+        getAddSongs();
+    }, [songlist]);
 
     return (
         <div className="playlist-page main-theme">
@@ -202,7 +308,7 @@ function PlaylistPage() {
                 /> */}
                 <h1 className="playlist-header">{viewingPlaylist == null ? "Loading..." : viewingPlaylist.title}</h1>
                 <div className="playlist-options">
-                    <AddIcon className="pointer add-song" fontSize="large" />
+                    <AddIcon className="pointer add-song" fontSize="large" onClick={openAddSongDialog} />
                     <DeleteIcon className="pointer delete-playlist" fontSize="large" onClick={openConfirmDeletePlaylist}/>
                 </div>
                 <Dialog
@@ -215,11 +321,41 @@ function PlaylistPage() {
                     }}>
                     <DialogTitle>Delete this playlist?</DialogTitle>
                     <DialogActions>
-                    <Button onClick={closeConfirmDeletePlaylist}>
+                        <Button onClick={closeConfirmDeletePlaylist}>
                             Cancel
                         </Button>
                         <Button onClick={deletePlaylist}>
                             Delete
+                        </Button>                        
+                    </DialogActions>
+
+                </Dialog>
+                <Dialog
+                    open={openAdd}
+                    onClose={closeAddSongDialog}
+                    PaperProps={{
+                        style: {
+                            borderRadius: "10px",
+                            minWidth: "70vw"
+                        },
+                    }}>
+                    <DialogTitle>Add Songs to Playlist</DialogTitle>
+                    <DialogContent>
+                        <AddSongList
+                            songs={addSongs}
+                            addSong={addSongToPlaylist}
+                            playlistId={playlistId}
+                            getSongs={getPlaylistSongs}
+                            playlistSongs={songs}
+                            setPlaylistSongs={setSongs}
+                            getAddSongs={getAddSongs}
+                            addSongs={addSongs}
+                            setAddSongs={setAddSongs}
+                             />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={closeAddSongDialog}>
+                            Done
                         </Button>                        
                     </DialogActions>
 
