669c596c386adcfd0510fd855edf2082f66482a5
Glenn Ye <glenn.ye38@gmail.com>
2021-04-11 18:09:20 -0400

    Hide delete song button for non-admin

--- front-end/src/components/DeleteSongButton.js
+++ front-end/src/components/DeleteSongButton.js
@@ -3,15 +3,18 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import { useDataLayerValue } from "../dataLayer";
 import "../styles.css";
 
-function DeleteSongButton({ song, setSongs, deleteSong, getSongs, playlistId }) {
+function DeleteSongButton({ song, setSongs, deleteSong, getSongs, playlistId, visible }) {
     const [, dispatch] = useDataLayerValue();
+    const style = { visibility: visible ? "visible" : "hidden" };
     // console.log("playtlist" + playlistId);
 
     return (
         <div className="delete-song-button">
             <DeleteIcon
                 className="pointer delete-icon"
-                onClick={() => playlistId != null ? deleteSong(song, setSongs, getSongs, playlistId) : deleteSong(song, setSongs, getSongs)} />
+                onClick={() => playlistId != null ? deleteSong(song, setSongs, getSongs, playlistId) : deleteSong(song, setSongs, getSongs)}
+                style={style}
+                />
         </div>
     )
 }
